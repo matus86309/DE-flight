176f13da8f42f478db8e90cd6e4627fd9a2915ee
BarnyKalmar <156135291+BarnyKalmar@users.noreply.github.com>
2026-03-17 18:21:36 +0100

    added inner product calculation for wind direction and strenght

--- parts/DE_extract.py
+++ parts/DE_extract.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas as pd
 from scipy.stats import linregress
+from scipy.stats import ttest_ind
 
 DB_PATH = "../data/flights_database.db"
 connection = sqlite3.connect(DB_PATH)
@@ -100,10 +101,10 @@ def get_flight_trajectory(departing_airport,arriving_airport):
     #get the tailnumber of all flights between selected airports
     query = f"""
             SELECT planes.type, COUNT(*)
-            FROM flight
+            FROM flights
             JOIN planes ON flights.tailnum = planes.tailnum
-            WHERE flights.origin = {departing_airport}
-                AND flights.destination = {arriving_airport}
+            WHERE flights.origin = '{departing_airport}'
+                AND flights.dest = '{arriving_airport}'
             GROUP BY planes.type"""
 
     cursor.execute(query)
@@ -135,10 +136,17 @@ def get_carrier_delay():
 
 
 def get_delayed_flight(month_range:[str],destinations_list:[str]):
+    #only use it with 'JFK'/'LAX' SQL ready format !
 
     #count the number of delayed flights in a given period
-    cursor.execute(f"SELECT COUNT(*) FROM flights WHERE arr_delay >0 AND month IN '{month_range}' AND dest IN '{destinations_list}';'")
-    num_days = cursor.fetchone()[0]
+    cursor.execute(f"""SELECT COUNT(*) 
+                   FROM flights 
+                   WHERE arr_delay > 0 
+                    AND month IN {month_range} 
+                    AND dest IN {destinations_list};""")
+
+    num_delays = cursor.fetchone()[0]
+    return num_delays
 
 
 
@@ -161,7 +169,7 @@ def get_top_manufacturers(destination_airport):
     return top_5_manufacturers
 
 
-def get_distance_delay_ratio():
+def get_distance_delay_regression():
 
     '''
     we calculate the regression coefficient to investigate how delay and flight distance correlate.
@@ -172,7 +180,7 @@ def get_distance_delay_ratio():
 
     slope, intercept, r_val, p_val, std_error = linregress(delay_df['distance'], delay_df['arr_delay'])
 
-    return f" Expected baseline delay: {intercept:.3} ; additional delay for every mile travelled: {slope:.3}"
+    return f" Expected baseline delay: {intercept:.3f} ; additional delay for every mile travelled: {slope:.3}"
 
 
 
@@ -204,12 +212,92 @@ def get_speed_for_model():
 
         #execute the query
         cursor.execute(write, (speed, model))
+
+
     #save the query once
     connection.commit()
 
 
+def get_wind_direction():
 
+    # get the wind_dir variable for all outbound flights from JFK
+    query = "SELECT wind_dir FROM weather WHERE origin = 'JFK' AND wind_dir IS NOT NULL;"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    directions = []
+
+    #convert the degrees into direction
+    for row in rows:
+        degrees  = row[0]
+        if degrees > 315 or degrees <= 45:
+            direction = "North"
+        elif degrees >45 and degrees <= 135:
+            direction = "East"
+        elif degrees >135 and degrees <= 225:
+            direction = "South"
+        else:
+            direction = "West"
+        directions.append(direction)
+    wind_dir = pd.DataFrame(directions, columns=["wind_dir"])
+    #get the direction dataframe
+    return wind_dir
+
+
+def get_inner_product(flight_id):
+
+    '''
+    get the merged day-month for the unique flight id, match it to the weather table on day-month object
+    return the inner product of the wind_dir degrees and the wind_speed
+    '''
+
+    query = f"""
+            SELECT flights.flight,
+                (flights.month || '-' || flights.day) AS daytime,
+                weather.wind_dir,
+                weather.wind_speed,
+                (weather.wind_speed * weather.wind_dir) AS inner_product
+            FROM flights
+            JOIN weather ON (flights.month || '-' || flights.day) = (weather.month || '-' || weather.day)
+            WHERE flights.flight = '{flight_id}';"""
+    cursor.execute(query)
+    inner_product= cursor.fetchone()
+
+    return inner_product[4]
+
+
+def wind_speed_innerprod_regression():
+
+    '''
+    this function gets a dataframe of all inner products of flights and their corresponding airtime
+    then sorts the inner product into positive or negative bucket
+    then it runs a T-test to see if the inner prod and air time have statistically signifiant correlation
+    '''
+
+    #get inner_prod, air_time
+    query= f"""
+            SELECT flights.air_time,
+                (weather.wind_speed * weather.wind_dir) AS inner_product
+            FROM flights
+            JOIN weather
+                ON (flights.month || '-' || flights.day) = (weather.month || '-' || weather.day)
+                AND flights.origin = weather.origin
+                AND flights.month = weather.month
+                AND flights.day = weather.day
+                AND flights.hour = weather.hour
+            WHERE flights.air_time IS NOT NULL
+                AND weather.wind_speed IS NOT NULL;"""
+
+    df = pd.read_sql(query, connection)
+
+    #split to positive negatibe
+    postive = df[df['inner_product'] >0]['air_time']
+    negative = df[df['inner_product'] < 0]['air_time']
+
+    #run the t-test
+    t_stat,p_value = ttest_ind(postive, negative, nan_policy = 'omit')
+
+    return f"there is {p_value:.3f} correlation between wind and airtime"
 
 
 connection.close()
-
